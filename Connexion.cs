48bb1b76f6cc0ec54b97dd3c816d7e18abe6d6e1
Salmandevelop <salman20060512@gmail.com>
2026-03-16 02:40:29 +0100

    Add files via upload

---
+++ Connexion.cs
@@ -0,0 +1,40 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+using System.Windows.Forms;
+
+namespace GestionQuestionnaires
+{
+    public class Connexion
+    {
+
+        private static string connectionString = "Server=localhost;Database=Stadium_Questionnaire;Uid=root;Pwd=toto;"; private static MySqlConnection cnx = null;
+
+        public static MySqlConnection getInstance()
+        {
+            if (cnx == null)
+            {
+                try
+                {
+                    cnx = new MySqlConnection(connectionString);
+                    cnx.Open();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Erreur Connexion : " + ex.Message);
+                }
+            }
+            else if (cnx.State == ConnectionState.Closed)
+            {
+                cnx.Open();
+            }
+            return cnx;
+        }
+
+        public static void Close()
+        {
+            if (cnx != null && cnx.State == ConnectionState.Open)
+                cnx.Close();
+        }
+    }
+}
